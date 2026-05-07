e0a10b136341e5024006ef176b48123a9639d601
Hizikyas Tamiru <hizikyastamiru@gmail.com>
2026-05-07 22:47:32 +0300

    Initial setup

---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+    server.GET("/ping" , func(context *gin.Context){
+		context.JSON(http.StatusOK , gin.H{"message" : "pong"})
+	})
+	server.Run()
+}
